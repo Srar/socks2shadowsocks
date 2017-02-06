f420a124fe9a4886b0cc4ef87c763d601d0f9e67
Srar <a1020938070@gmail.com>
2017-02-07 02:01:35 +0800

    增加事件通知

--- src/Socks5SSProxy.ts
+++ src/Socks5SSProxy.ts
@@ -2,45 +2,32 @@
 
 import * as net from "net"
 import * as crypto from "crypto";
+import * as events from "events";
 
 import SSCrypto from "./Crypto/SSCrypto";
 import { ISSCryptoMethod } from "./Crypto/ISSCryptoMethod";
 
-import ProxyProcess, { ProxyProcessConfig } from "./Socks5SSProxyProcess";
+import Socks5SSProxyProcess, { Socks5SSProxyProcessConfig } from "./Socks5SSProxyProcess";
 
-class Socks5SSProxyServer {
+export default class Socks5SSProxy extends events.EventEmitter {
 
     private readonly proxyServer: net.Server = null;
-
-    isListen: boolean = false;
-    upload: number = 0;
-    download: number = 0;
-    processes: Array<ProxyProcess> = [];
+    private processes: Array<Socks5SSProxyProcess> = [];
 
     constructor(
         private readonly localPort: number,
         private readonly targetHost: string,
-        private readonly targetPort: number
+        private readonly targetPort: number,
+        private readonly ssMethod: string,
+        private readonly ssPassword: string,
     ) {
+        super();
         this.proxyServer = net.createServer(this.onClientConnect.bind(this));
+        this.proxyServer.on("error", (err) => this.emit("error", err));
     }
 
     listen() {
-        this.proxyServer.listen(this.localPort, () => {
-            this.isListen = true;
-        });
-
-        setInterval(function() {
-            console.log(this.processes.length);
-        }.bind(this), 1000);
-
-        // setInterval(function () {
-        //     var uploadSpeed: number = this.upload / 1024;
-        //     var downloadSpeed: number = this.download / 1024;
-        //     this.upload   = 0;
-        //     this.download = 0;
-        //     console.log(`uploadSpeed: ${uploadSpeed.toFixed(0)}kb/s   downloadSpeed:${downloadSpeed.toFixed(0)}kb/s`);
-        // }.bind(this), 1000);
+        this.proxyServer.listen(this.localPort);
     }
 
     close() {
@@ -49,40 +36,12 @@ class Socks5SSProxyServer {
     }
 
     private onClientConnect(client: net.Socket) {
-
-        var process = new ProxyProcess({
+        var process: Socks5SSProxyProcess = new Socks5SSProxyProcess({
             targetHost: this.targetHost,
             targetPort: this.targetPort,
             clientSocket: client,
-            encryptProcess: SSCrypto.createCryptoMethodObject("rc4-md5", "9VNNPzCkV4LcuGd"),
-
-            onConnect: (targetAddress: string) => {
-                this.processes.push(process);
-                console.log("傻逼连接到:", targetAddress, this.processes.length);
-            },
-
-            onClose: () => {
-                var index = this.processes.indexOf(process);
-                if (index > -1) {
-                    this.processes.splice(index, 1);
-                }
-                console.log("傻逼断开了", this.processes.length);
-            },
-
-            onError: (err: Error) => {
-                console.log("傻逼爆炸了:", err.message);
-            },
-
-            onUploadTraffic: (traffic: number) => {
-                this.upload += traffic;
-            },
-
-            onDownloadTraffic: (traffic: number) => {
-                this.download += traffic;
-            },
+            encryptMethod: SSCrypto.createCryptoMethodObject(this.ssMethod, this.ssPassword),
         });
+        this.emit("clientConnected", process);
     }
 }
-
-var proxy = new Socks5ToShadowsocksProxyServer(5000, "192.168.0.250", 22);
-proxy.listen();
--- src/Socks5SSProxyProcess.ts
+++ src/Socks5SSProxyProcess.ts
@@ -1,39 +1,38 @@
 import * as net from "net"
 import * as crypto from "crypto";
+import * as events from "events";
 
 import SSCrypto from "./Crypto/SSCrypto";
 import { ISSCryptoMethod } from "./Crypto/ISSCryptoMethod";
 
-export default class ProxyProcess {
+export default class Socks5SSProxyProcess extends events.EventEmitter {
 
-    readonly initTime: number = new Date().getTime();
+    private readonly initTime: number = new Date().getTime();
+    private firstTrafficTime: number = 0;
 
-    readonly clientSocket: net.Socket;
-    readonly targetSocket: net.Socket;
-    readonly clientIP: string;
-    readonly clientPort: number;
+    private readonly clientSocket: net.Socket;
+    private readonly targetSocket: net.Socket;
 
+    private dataBuffer: Buffer = new Buffer([]);
+    private isConnectTarget: boolean = false;
+    private isClear: boolean = false;
+    private isFirstTraffic: boolean = true;
 
-    dataBuffer: Buffer = new Buffer([]);
-    isConnectTarget: boolean = false;
-    isClear: boolean = false;
-    isFirstTraffic: boolean = true;
-    targetAddress: string = "";
+    private remoteAddress: string = "";
+    private remotePort: number = 0;
 
-    socks5HandSetup: number = 0;
+    private socks5HandSetup: number = 0;
 
     private isClientFirstPackage: boolean = true;
     private isTargetFirstPackage: boolean = true;
 
-    constructor(private processConfig: ProxyProcessConfig) {
+    constructor(private processConfig: Socks5SSProxyProcessConfig) {
+        super();
         this.clientSocket = processConfig.clientSocket;
         this.clientSocket.setNoDelay(false);
         this.clientSocket.on("data", this.onClientSocketData.bind(this));
         this.clientSocket.on("close", this.onClientSocketClose.bind(this));
         this.clientSocket.on("error", this.onClientSocketError.bind(this));
-
-        this.clientIP = this.clientSocket.address().address;
-        this.clientPort = this.clientSocket.address().port;
 
         this.targetSocket = new net.Socket();
         this.targetSocket.setNoDelay(false);
@@ -60,61 +59,58 @@ export default class ProxyProcess {
             return;
         } else if (this.socks5HandSetup == 1) {
             if (data[1] != 0x00) {
-                console.log("Socks5握手失败");
-                console.log(data);
-                return this.clearConnect();
+                return this.onTargetSocketError(new Error("Socks5握手失败 数据包:" + JSON.stringify(data)));
             }
             // console.log("Socks5握手成功");
             // console.log(this.dataBuffer.toString());
             this.targetSocket.write(this.dataBuffer);
             this.dataBuffer = null;
             this.isConnectTarget = true;
-            if (this.processConfig.onConnect) {
-                this.processConfig.onConnect(this.targetAddress);
-            }
+            this.emit("socks5Connected");
             this.socks5HandSetup++;
             return;
         }
 
         if (this.isFirstTraffic) {
             this.isFirstTraffic = false;
+            /* 记录首次通讯时间 */
+            this.firstTrafficTime = new Date().getTime();
+            /* 触发首次通讯事件 */
+            this.emit("firstTraffic", this.firstTrafficTime - this.initTime);
         }
 
         // console.log("==Socks5==");
         // console.log(data.toString());
         // console.log("==========");
 
-        if (this.processConfig.onUploadTraffic) {
-            this.processConfig.onUploadTraffic(data.length);
-        }
+        this.emit("socks5Data", data);
 
-        data = this.processConfig.encryptProcess.encryptData(data);
+        data = this.processConfig.encryptMethod.encryptData(data);
         this.clientSocket.write(data);
     }
 
     private onClientSocketData(data: Buffer) {
         try {
-            data = this.processConfig.encryptProcess.decryptData(data);
+            data = this.processConfig.encryptMethod.decryptData(data);
         } catch (error) {
             this.onClientSocketError(error);
             return;
         }
         if (this.isClientFirstPackage) {
-            //data = data.slice(this.processConfig.encryptProcess.getEncryptConfig().ivLengrh);
             var address: string = "";
             var addressLength: number = 0;
             var addressType: "Unknow" | "IPv4" | "IPv6" | "Domain" = "Unknow";
-            if (data[0] == 0x01) {
+            if (data[0] == 0x03) {
+                addressType = "Domain";
+                addressLength = data[1];
+                address = data.slice(1, addressLength + 2).toString();
+            } else if (data[0] == 0x01) {
                 addressType = "IPv4";
                 addressLength = 4;
                 address += data[1].toString() + ".";
                 address += data[2].toString() + ".";
                 address += data[3].toString() + ".";
                 address += data[4].toString();
-            } else if (data[0] == 0x03) {
-                addressType = "Domain";
-                addressLength = data[1];
-                address = data.slice(1, addressLength + 2).toString();
             } else if (data[0] == 0x04) {
                 addressType = "IPv6";
                 addressLength = 16
@@ -135,22 +131,20 @@ export default class ProxyProcess {
                 address += data[15].toString() + ":";
                 address += data[16].toString();
             } else {
-                return this.onClientSocketError(new Error(`${this.clientIP}:${this.clientPort} 发送了未知的数据包.`))
+                return this.onClientSocketError(new Error(`${this.clientSocket.address().address}:${this.clientSocket.address().port} 发送了未知的数据包.`))
             }
-            this.targetAddress = address + ":" + ((data[addressLength + 2] << 8) + data[addressLength + 3]);
-            this.targetAddress = this.targetAddress.trim();
+            this.remoteAddress = address;
+            this.remotePort = ((data[addressLength + 2] << 8) + data[addressLength + 3]);
+            this.remoteAddress = this.remoteAddress.trim();
             data = Buffer.concat([new Buffer([0x05, 0x01, 0x00]), data]);
             this.isClientFirstPackage = false;
         }
-
 
         // console.log("==iPhone==");
         // console.log(data.toString());
         // console.log("==========");
 
-        if (this.processConfig.onDownloadTraffic) {
-            this.processConfig.onDownloadTraffic(data.length);
-        }
+        this.emit("clientData", data);
 
         /*
             判断是否已经连接至Socks5服务器  
@@ -173,17 +167,29 @@ export default class ProxyProcess {
     }
 
     private onClientSocketError(error: Error) {
-        if (this.processConfig.onError) {
-            this.processConfig.onError(error);
-        }
+        this.emit("error", error);
         this.clearConnect();
     }
 
     private onTargetSocketError(error: Error) {
-        if (this.processConfig.onError) {
-            this.processConfig.onError(error);
-        }
+        this.emit("error", error);
         this.clearConnect();
+    }
+
+    public getRemoteAddress(): string {
+        return this.remoteAddress;
+    }
+
+    public getRemotePort(): number {
+        return this.remotePort;
+    }
+
+    public getClientSocket(): net.Socket {
+        return this.clientSocket;
+    }
+
+    public getTargetSocket(): net.Socket {
+        return this.targetSocket;
     }
 
     public clearConnect() {
@@ -198,22 +204,14 @@ export default class ProxyProcess {
             this.clientSocket.destroy();
         } catch (ex) { }
         this.dataBuffer = null;
-        if (this.processConfig.onClose && this.isConnectTarget) {
-            this.processConfig.onClose();
-        }
+        this.emit("close");
+        this.removeAllListeners();
     }
 }
 
-export interface ProxyProcessConfig {
+export interface Socks5SSProxyProcessConfig {
     targetHost: string;
     targetPort: number;
     clientSocket: net.Socket;
-    encryptProcess: ISSCryptoMethod;
-
-    /* Event */
-    onConnect?: Function;
-    onClose?: Function;
-    onError?: Function;
-    onUploadTraffic?: Function;
-    onDownloadTraffic?: Function;
+    encryptMethod: ISSCryptoMethod;
 }
