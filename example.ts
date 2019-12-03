ec70e083db7a89f59680d21ad7f08e3dcb5f888a
Srar <a1020938070@gmail.com>
2019-12-03 10:28:03 +0800

    更新typescript版本

--- example.ts
+++ example.ts
@@ -1,24 +1,9 @@
-///<reference path="./node_modules/@types/node/index.d.ts"/>
-
 import Socks5SSProxy from "./src/Socks5SSProxy";
 import Socks5SSProxyTcpProcess from "./src/Socks5SSProxyTcpProcess";
 
-import SSCrypto from "./src/Crypto/SSCrypto";
-
-/* listen port 3389 of Shadowsocks server and traffic forward to port 22 of Socks5 server. */
+/* Socks5监听3389端口并将请求转换至192.168.0.250:9257Shadowsocks端口. */
 var proxy: Socks5SSProxy = new Socks5SSProxy(3389, "192.168.0.250", 9257, "rc4-md5", "123456");
 var processes: Array<Socks5SSProxyTcpProcess> = [];
-
-var upload: number = 0;
-var download: number = 0;
-
-setInterval(function () {
-    var uploadSpeed: number = upload / 1024;
-    var downloadSpeed: number = download / 1024;
-    upload = 0;
-    download = 0;
-    console.log(`uploadSpeed: ${uploadSpeed.toFixed(0)}kb/s   downloadSpeed:${downloadSpeed.toFixed(0)}kb/s`);
-}.bind(this), 1000);
 
 proxy.on("clientConnected", (p: Socks5SSProxyTcpProcess) => {
 
@@ -29,16 +14,15 @@ proxy.on("clientConnected", (p: Socks5SSProxyTcpProcess) => {
     p.on("firstTraffic", (time: number) => {
         var remoteAddress: string = `${p.getRemoteAddress()}:${p.getRemotePort()}`;
         var clientAddress: string = `${p.getClientSocket().remoteAddress}:${p.getClientSocket().remotePort}`;
-        console.log(`Client [${clientAddress}] connected to [${remoteAddress}]. Usage time: ${time}ms`);
+        console.log(`Client [${clientAddress}] connected to [${remoteAddress}].`);
     });
 
     p.on("socks5Data", (data: Buffer) => {
-        upload += data.length;
+        
     });
 
     var checkedAddress = false;
     p.on("clientData", (data: Buffer) => {
-
         if (!checkedAddress) {
             var addressBlockList: Array<string> = [
                 "api.map.baidu.com",
@@ -61,8 +45,6 @@ proxy.on("clientConnected", (p: Socks5SSProxyTcpProcess) => {
             }
             checkedAddress = true;
         }
-        /* 记录Shadowsocks客户端上行流量 */
-        download += data.length;
     });
 
     p.on("close", () => {
